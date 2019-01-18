7f4e0fa2cd5173ea4be0d25620aed9c33db3a43d
tangzhengming <1085265828@qq.com>
2019-01-18 21:06:09 +0800

    注册页button按钮

--- dickies/js/zhuce.js
+++ dickies/js/zhuce.js
@@ -1,8 +1,10 @@
  //引入头部
+/* 
  $(function(){
      $('#header').load('./header.html');
      $('#foot').load('./weibu.html');
    })
+*/   
    //DOM操作 获得焦点失去焦点事件
  /*var inputs=document.querySelectorAll("form>div.biao>div.label_input>div>input")
  for(var input of inputs){
@@ -41,15 +43,42 @@
    }
  })
 
-
- $("button").click(function(e){
+//点击button按钮
+/*$("button").click(function(e){
+    e.preventDefault();
+    $.each($(".label_input>div>input"),function(i,elem){
+        if($(elem).val()==""){
+            $(elem).addClass("input_red");
+        }else{
+            $(elem).addClass("input_black");
+        }
+    })
+})*/
+//button按钮加载input框
+$("button").click(function(e){
   e.preventDefault();
-   var $inp=$(":text");
-   if($inp.val()==""){
-     $inp.addClass("input_red")
-     .next().html("这是必填项")
-   }else{
-     $inp.removeClass()
-     .next().html("");
-   }
+  $(":text").each(function(){
+  if($(this).val()==""){
+    $(this).addClass("input_red")
+    .next().html("这是必填项");
+  }else{
+    $(this).removeClass()
+    .next().html("");
+  }
  })
+})
+
+function vail(txt,reg,html){
+  if(!reg.test(txt.val())){
+     txt.prev().css("color","#c41230")
+     .next().addClass("input_red")
+     .next().html(html)  
+  }else{
+    txt.prev().css("color","#444")
+    .next().removeClass()
+    .next().html("");
+  }
+}
+$("button").on(click,function(){
+  
+})
