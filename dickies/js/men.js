68349f273ff66ec16c584a30d6b01c8071476672
tangzhengming <1085265828@qq.com>
2019-02-23 13:55:11 +0800

    细微改动

--- dickies/js/men.js
+++ dickies/js/men.js
@@ -1,4 +1,15 @@
 $(function(){
     $("#header").load("./header.html")
     $("#footer").load("./weibu.html")
+    $(".man-top span:last-child").click(function(){
+        var $span=$(this);
+        if($span.parent().next().css("display")=="none"){
+          $span.css("transform","rotate(180deg)")
+          .parent().next().css("display","block");
+        }else{
+          $span.css("transform","rotate(0deg)")
+          .parent().next().css("display","none");
+        }
+    })
+    
 })
